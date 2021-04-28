59d78241ebba9e6fcf95e804ae672307ab0e5bd1
aaroluo <aaro.luo@gmail.com>
2021-04-28 16:19:12 +0300

    app only flow in progress

--- lib/src/oauth_client.dart
+++ lib/src/oauth_client.dart
@@ -2,12 +2,16 @@ import 'package:flutter_reddit/src/const.dart';
 import 'package:http/http.dart' as http;
 import 'package:oauth2_client/access_token_response.dart';
 import 'package:oauth2_client/oauth2_client.dart';
-import 'package:random_string/random_string.dart';
+import 'package:uuid/uuid.dart';
+// ignore: implementation_imports
+import 'package:oauth2_client/src/token_storage.dart';
 
 class RedditOAuthClient extends OAuth2Client {
+  final TokenStorage storage;
   RedditOAuthClient({
     required String redirectUri,
     required String customUriScheme,
+    required this.storage,
   }) : super(
           authorizeUrl: AUTHORIZE_URL,
           tokenUrl: TOKEN_URL,
@@ -16,6 +20,20 @@ class RedditOAuthClient extends OAuth2Client {
         );
 
   Future<AccessTokenResponse> getTokenWithAppOnlyFlow() async {
+    final deviceId = await _getDeviceId();
+
     return AccessTokenResponse.fromMap({});
+  }
+
+  Future<String> _getDeviceId() async {
+    final token = await storage.storage.read('deviceId');
+    if (token != null) {
+      return token;
+    } else {
+      print('generated device ID for authentication');
+      final token = Uuid().v1();
+      storage.storage.write('deviceId', token);
+      return token;
+    }
   }
 }
